8598a444f6eb164c8116ee4f5ee8738df1d06f84
lvmil <mohammad.mola1385@gmail.com>
2025-08-11 11:33:21 +0200

    Delete stackblitz-starters-nkwxpmhz-3 directory

--- stackblitz-starters-nkwxpmhz-3/lib/fetchers/aldi.ts
+++
@@ -1,7 +0,0 @@
-import { Deal } from "../types";
-
-export async function fetchAldi(zip: string): Promise<Deal[]> {
-  return [
-    { id: `aldi-1`, store: "aldi", title: "Basmati rice 1kg", price: 1.79, unit: "€" },
-  ];
-}
--- stackblitz-starters-nkwxpmhz-3/lib/fetchers/edeka.ts
+++
@@ -1,7 +0,0 @@
-import { Deal } from "../types";
-
-export async function fetchEdeka(zip: string): Promise<Deal[]> {
-  return [
-    { id: `edeka-1`, store: "edeka", title: "Skyr 500g", price: 1.29, unit: "€" },
-  ];
-}
