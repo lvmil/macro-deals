687fbc996a107c6cd8ed5940bf3cc84635f65c00
lvmil <mohammad.mola1385@gmail.com>
2025-08-11 11:31:12 +0200

    Delete macro-deals directory

--- macro-deals/lib/fetchers/aldi.ts
+++
@@ -1,7 +0,0 @@
-import { Deal } from '../types';
-export async function fetchAldi(zip: string): Promise<Deal[]> {
-  return [
-    { id: 'aldi-stub-1-' + zip, store: 'aldi', title: 'Aldi Sample A', price: 0.89, unit: 'EUR' },
-    { id: 'aldi-stub-2-' + zip, store: 'aldi', title: 'Aldi Sample B', price: 1.39, unit: 'EUR' },
-  ];
-}
--- macro-deals/lib/fetchers/lidl.ts
+++
@@ -1,7 +0,0 @@
-import { Deal } from '../types';
-export async function fetchLidl(zip: string): Promise<Deal[]> {
-  return [
-    { id: 'lidl-stub-1-' + zip, store: 'lidl', title: 'Lidl Sample A', price: 0.99, unit: 'EUR' },
-    { id: 'lidl-stub-2-' + zip, store: 'lidl', title: 'Lidl Sample B', price: 1.49, unit: 'EUR' },
-  ];
-}
